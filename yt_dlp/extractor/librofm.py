b608006684997e6e97bec108473785a3f3000bdd
Naglis Jonaitis <827324+naglis@users.noreply.github.com>
2024-08-30 10:53:17 +0300

    wip

--- yt_dlp/extractor/librofm.py
+++ yt_dlp/extractor/librofm.py
@@ -1,22 +1,32 @@
 import json
 
 from .common import InfoExtractor
-from ..utils import urljoin, parse_iso8601, traverse_obj, join_nonempty
+from ..utils import (
+    float_or_none,
+    int_or_none,
+    join_nonempty,
+    parse_iso8601,
+    str_or_none,
+    strip_or_none,
+    traverse_obj,
+    url_or_none,
+    urljoin,
+)
 
 
 class LibroFmIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?libro\.fm/audiobooks/(?P<isbn>[0-9]{13})(?:-(?P<display_id>[a-z0-9-]+))?'
     _NETRC_MACHINE = 'librofm'
-    _BASE_URL = "https://libro.fm"
-    _LOGIN_URL = f"{_BASE_URL}/oauth/token"
-    _USER_AGENT = "okhttp/3.14.9"
+    _BASE_URL = 'https://libro.fm'
+    _LOGIN_URL = f'{_BASE_URL}/oauth/token'
+    _USER_AGENT = 'okhttp/3.14.9'
     _USER_AGENT_DOWNLOAD = (
-        "AndroidDownloadManager/11 (Linux; U; Android 11; "
-        "Android SDK built for x86_64 Build/RSR1.210722.013.A2)"
+        'AndroidDownloadManager/11 (Linux; U; Android 11; '
+        'Android SDK built for x86_64 Build/RSR1.210722.013.A2)'
     )
     _CLIENT_VERSION = (
-        "Android: Libro.fm 7.6.1 Build: 194 Device: Android SDK built for x86_64 "
-        "(unknown sdk_phone_x86_64) AndroidOS: 11 SDK: 30"
+        'Android: Libro.fm 7.6.1 Build: 194 Device: Android SDK built for x86_64 '
+        '(unknown sdk_phone_x86_64) AndroidOS: 11 SDK: 30'
     )
     _TESTS = [{
         'url': 'https://libro.fm/audiobooks/9781250761170-the-design',
@@ -37,43 +47,51 @@ class LibroFmIE(InfoExtractor):
             #     'tags': 'count:10',
             # * Any Python type, e.g.
             #     'view_count': int,
-        }
+        },
     }]
 
     _ACCESS_TOKEN = None
 
     def _perform_login(self, username, password):
-        if username == "__token__":
+        if username == 'token':
             self._ACCESS_TOKEN = password
         else:
-            login_json = self._download_json(self._LOGIN_URL, None, note='Logging in', errnote='Login failed', headers={
-                'Content-Type': 'application/json',
-                'User-Agent': self._USER_AGENT,
-            },
-            data=json.dumps({
-                "grant_type": "password",
-                "username": username,
-                "password": password,
-            }).encode())
-            access_token = login_json["access_token"]
-            self.to_screen(f'The access token is {access_token!r}. Use it and "__token__" as the username to avoid login requests.')
+            login_json = self._download_json(
+                self._LOGIN_URL,
+                None,
+                note='Logging in',
+                errnote='Login failed',
+                headers={
+                    'Content-Type': 'application/json',
+                    'User-Agent': self._USER_AGENT,
+                },
+                data=json.dumps({
+                    'grant_type': 'password',
+                    'username': username,
+                    'password': password,
+                }).encode())
+
+            access_token = login_json['access_token']
+            self.to_screen(
+                f'The access token is {access_token!r}. '
+                'Use it and "token" as the username to avoid login requests.')
             self._ACCESS_TOKEN = access_token
 
     def _process_part(self, part):
         return {
             'formats': [{
-                 'url': part['url'],
-                 'ext': 'temp',
-                 'format': 'zip archive of mp3 files',
-                 # 'acodec': 'mp3',
-                 # 'container': 'zip',
-                 'filesize': part['size_bytes'],
-                 'http_headers': {
-                     'User-Agent': self._USER_AGENT_DOWNLOAD,
-                 },
-            }]
+                **traverse_obj(part, {
+                    'url': ('url', url_or_none),
+                    'filesize': ('size_bytes', int_or_none),
+                }),
+                'ext': 'temp',
+                'format': 'zip archive of mp3 files',
+                'http_headers': {
+                    'User-Agent': self._USER_AGENT_DOWNLOAD,
+                },
+            }],
         }
-        
+
     def _real_extract(self, url):
         isbn, display_id = self._match_valid_url(url).group('isbn', 'display_id')
 
@@ -85,42 +103,41 @@ class LibroFmIE(InfoExtractor):
             'User-Agent': self._USER_AGENT,
         }, note='Downloading audiobook details JSON', errnote='Failed to download audiobook details JSON')
 
-        audiobook_data = traverse_obj(details, ('data', 'audiobook'))
-        audiobook_info = audiobook_data.get('audiobook_info', {})
-
-        thumbnail = None
-        if (cover_url := audiobook_data.get("cover_url")):
-            thumbnail = self._proto_relative_url(cover_url)
-
         info = {
             'id': isbn,
-            'title': audiobook_data["title"],
-            'alt_title': audiobook_data.get('subtitle'),
             'display_id': display_id,
-            'thumbnail': thumbnail,
-            'description': audiobook_data.get("description"),
-            # XXX: Does this make sense (publisher being used as the value for uploader)?
-            'uploader': audiobook_data.get('publisher'),
-            'creators': audiobook_data.get('authors') or [],
-            'timestamp': parse_iso8601(audiobook_data.get('created_at')),
-            'release_timestamp': parse_iso8601(audiobook_data.get('publication_date')),
-            'modified_timestamp': parse_iso8601(audiobook_data.get('updated_at')),
-            'duration': audiobook_info.get('duration'),
-            'categories': [g['name'] for g in audiobook_data.get('genres') or []],
-            'cast': audiobook_info.get('narrators') or [],
+            **traverse_obj(
+                details,
+                ('data', 'audiobook', {
+                    'title': ('title', {str_or_none}),
+                    'alt_title': ('subtitle', {str_or_none}),
+                    'thumbnail': ('cover_url', {self._proto_relative_url}),
+                    'description': ('description', {strip_or_none}),
+                    # XXX: Does this make sense (publisher being used as the value for channel)?
+                    'channel': ('publisher', {str}),
+                    'creators': ('authors', {str}),
+                    'timestamp': ('created_at', {parse_iso8601}),
+                    'release_timestamp': ('publication_date', {parse_iso8601}),
+                    'modified_timestamp': ('updated_at', {parse_iso8601}),
+                    'duration': ('duration', {float_or_none}),
+                    'categories': ('genres', ..., 'name', {str}),
+                    'cast': ('audiobook_info', 'narrators', {str}),
+                    # XXX: Does this make sense (series and season number)?
+                    'series': ('series', {str_or_none}),
+                    'season_number': ('series_num', {int_or_none}),
+                }),
+            ),
         }
 
-        # Does this make sense?
-        if (series := audiobook_data.get('series')):
-            info['series'] = series
-
-            if (series_num := audiobook_data.get('series_num')) is not None:
-                info['season_number'] = series_num
-
-        manifest = self._download_json(urljoin(self._BASE_URL, "/api/v9/download-manifest"), isbn, query={'isbn': isbn, 'client_version': self._CLIENT_VERSION}, headers={
-            'Authorization': f'Bearer {self._ACCESS_TOKEN}',
-            'User-Agent': self._USER_AGENT,
-        }, note='Downloading download manifest JSON', errnote='Failed to download download manifest JSON')
+        manifest = self._download_json(
+            urljoin(self._BASE_URL, '/api/v9/download-manifest'),
+            isbn,
+            query={'isbn': isbn, 'client_version': self._CLIENT_VERSION},
+            headers={
+                'Authorization': f'Bearer {self._ACCESS_TOKEN}',
+                'User-Agent': self._USER_AGENT,
+            }, note='Downloading download manifest JSON',
+            errnote='Failed to download download manifest JSON')
 
         parts = traverse_obj(manifest, ('parts', ...))
         entries = [{
@@ -134,7 +151,7 @@ class LibroFmIE(InfoExtractor):
             **info,
             **entries[0],
             'id': isbn,
-            'title': info.get('title')
+            'title': info.get('title'),
         } if len(entries) == 1 else {
             '_type': 'multi_video',
             **info,
--- yt_dlp/postprocessor/extractzip.py
+++ yt_dlp/postprocessor/extractzip.py
@@ -4,5 +4,9 @@ from .common import PostProcessor
 class ExtractZipPP(PostProcessor):
 
     def run(self, information):
-        import pdb; pdb.set_trace()
+        # We only run when the extension is ".temp", since ".zip" is not allowed by yt-dlp.
+        if information['ext'] != 'temp':
+            return [], information
+
+        # filepath = information['filepath']
         return [], information

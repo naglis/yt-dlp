bc58072fa8ee37c3be2943756396a6398a9b91e8
naglis <827324+naglis@users.noreply.github.com>
2024-09-15 17:05:34 +0300

    wip

--- yt_dlp/extractor/extrememusic.py
+++ yt_dlp/extractor/extrememusic.py
@@ -1,7 +1,10 @@
 import json
+import re
+import functools
+import copy
 
 from .common import InfoExtractor
-from ..utils import ExtractorError, traverse_obj, str_or_none, int_or_none, url_or_none
+from ..utils import ExtractorError, traverse_obj, str_or_none, int_or_none, url_or_none,float_or_none
 
 
 class ExtremeMusicAlbumIE(InfoExtractor):
@@ -37,9 +40,8 @@ class ExtremeMusicAlbumIE(InfoExtractor):
                 "height": h,
             }
 
-                
         thumbnails = []
-        for image_type, images in (info.get('images') or {}).items():
+        for image_type, images in (info.get("images") or {}).items():
             for image in images:
                 thumbnails.append(prepare_thumbnail(image_type, image))
                 thumbnails.append(prepare_thumbnail(image_type, image, webp=True))
@@ -49,14 +51,54 @@ class ExtremeMusicAlbumIE(InfoExtractor):
             for thumb_id in ("detail", "large", "small"):
                 url = url_or_none(info.get(f"image_{thumb_id}_url"))
                 if url is not None and url not in seen_urls:
-                    thumbnails.append({
-                        "id": thumb_id,
-                        "url": url,
-                    })
+                    thumbnails.append(
+                        {
+                            "id": thumb_id,
+                            "url": url,
+                        }
+                    )
                     seen_urls.add(url)
 
         return thumbnails
+
+    @staticmethod
+    def _get_hls_stream_url(url: str|None)->str|None:
+        # Regex patterns copied verbatim from extrememusic.com, except for porting backreferences.
+        if url is None or not re.match(r'^(.*)\.m3u8(\?.*)?$', url):
+            return None
+
+        if re.match(r'^(.*)\/HLS\/\d{1,}(_v\d)?\.m3u8(\?.*)?$', url):
+            return url
+        return re.sub(r'^(.*)\.m3u8(\?.*)?$', r'\1/HLS/128_v4.m3u8', url)
+
+    @staticmethod
+    def _get_dash_stream_url(url: str|None)->str|None:
+        # Regex patterns copied verbatim from extrememusic.com, except for porting backreferences.
+        print(url)
+        if url is None or not re.match(r'^(.*)\.mpd(\?.*)?$', url):
+            return None
+
+        if re.match(r'^(.*)\/DASH\.mpd(\?.*)?$', url):
+            return url
+        return re.sub(r'^(.*)\.mpd(\?.*)?$', r'\1/DASH.mpd\2', url)
+
+    def _extract_asset_formats(self, video_id, asset):
+        formats = []
+        if url := url_or_none(asset.get("preview_url")):
+            formats.append({
+                "url": url,
+                "vcodec": "none",
+            })
+
+        if url := self._get_dash_stream_url(url_or_none(asset.get("preview_url_dash"))):
+            print(url)
+            formats.extend(self._extract_mpd_formats(url, video_id, fatal=False))
+
+        if url := self._get_hls_stream_url(url_or_none(asset.get("preview_url_hls"))):
+            formats.extend(self._extract_m3u8_formats(url, video_id, fatal=False))
             
+        return formats
+
     def _real_extract(self, url):
         album_id = self._match_id(url)
         webpage = self._download_webpage(url, album_id)
@@ -76,28 +118,45 @@ class ExtremeMusicAlbumIE(InfoExtractor):
 
         track_sounds = {}
         for ts in album_info.get("track_sounds") or []:
-            track_sounds[ts['id']] = ts
+            track_sounds[ts["id"]] = ts
 
         entries = []
         for track in album_info.get("tracks") or []:
             entry = {
-                **traverse_obj(track, {
-                    'id': ('id', {int_or_none}),
-                    'track': ('title', {str_or_none}),
-                    'thumbnails': ({self._collect_images},),
-                    'tags': ('keywords', ..., 'label', {str_or_none}),
-                    'artists': ('artists', ..., 'name', {str_or_none}),
-                    'composers': ('composers', ..., 'name', {str_or_none}),
-                    'genres': ('genre', ..., 'label', {str_or_none}),
-                }),
-                # 'thumbnails': self._collect_images(track),
+                **traverse_obj(
+                    track,
+                    {
+                        "id": ("id", {int_or_none}),
+                        "title": ("title", {str_or_none}),
+                        "track": ("title", {str_or_none}),
+                        "thumbnails": ({self._collect_images},),
+                        "tags": ("keywords", ..., "label", {str_or_none}),
+                        "artists": ("artists", ..., "name", {str_or_none}),
+                        "composers": ("composers", ..., "name", {str_or_none}),
+                        "genres": ("genre", ..., "label", {str_or_none}),
+                    },
+                ),
             }
-            entries.append(entry)
+            for track_sound_id in track.get("track_sound_ids") or []:
+                sound_entry = copy.deepcopy(entry)
+                track_sound = track_sounds[track_sound_id]
+                combined_id = f"{entry['id']}-{track_sound['id']}"
+                sound_entry.update({
+                    "id": combined_id,
+                    "title": f"{entry['title']} ({track_sound['version_name']})",
+                    "track": f"{entry['track']} ({track_sound['version_name']})",
+                    **traverse_obj(track_sound, {
+                        "duration": ("duration", {float_or_none}),
+                        "formats": ("assets", "audio", {functools.partial(self._extract_asset_formats, combined_id)}),
+                    }),
+                })
+                entries.append(sound_entry)
+
         import pprint
 
         pprint.pprint(album_info)
 
-        r= self.playlist_result(
+        r = self.playlist_result(
             entries,
             playlist_id=album_id,
             **traverse_obj(
@@ -105,12 +164,12 @@ class ExtremeMusicAlbumIE(InfoExtractor):
                 {
                     "playlist_title": ("album", "title", {str_or_none}),
                     "playlist_description": ("album", "description", {str_or_none}),
-                    "playlist_count": ("album", "track_count", {int_or_none}),
+                    # "playlist_count": ("album", "track_count", {int_or_none}),
                     "thumbnails": ("album", {self._collect_images}),
                 },
             ),
         )
-        print(r)
+        # print(r)
         return r
         # return {
         #     "id": album_id,
